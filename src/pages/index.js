348655c287b15e4fefd6ab81b700328e2f0351d7
danaharley <danaharley.github@gmail.com>
2021-01-02 13:14:46 +0700

    Add CountriesTable Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 import Layout from "../components/Layout/Layout";
 import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
@@ -9,6 +10,8 @@ export default function Home({ countries }) {
       <div className={styles.counts}>Found {countries.length} countries</div>
 
       <SearchInput placeholder="Search by name, region or subregion" />
+
+      <CountriesTable countries={countries} />
     </Layout>
   );
 }
